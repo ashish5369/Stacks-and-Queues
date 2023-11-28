6b0490c565f724f666d70d176a2d82c5a8568113
Your Name <you@example.com>
2023-11-29 02:39:09 +0530

    day6

---
+++ minStack.java
@@ -0,0 +1,6 @@
+// find the minimum element of any stack in o(1)
+// we can havre two stacks, one stack will only store the number smaller than
+// the top element in the min stack
+// here the time is O(1) but the space is O(n)
+
+// or we can have one min variabkle , here we have time and space both of O(1)
